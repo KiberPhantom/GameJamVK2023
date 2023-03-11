921b6a28d2d2b492270a0a3d7ca1c7fe2e7615c0
KiberPhantom <100571463+KiberPhantom@users.noreply.github.com>
2023-03-11 15:56:41 +0900

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,16 +1,16 @@
 // Подписывается на события, отправленные нативным клиентом
-bridge.subscribe((e) => console.log(e)); 
+vkBridge.subscribe((e) => console.log(e)); 
     
 // Отправляет событие нативному клиенту
-bridge.send("VKWebAppInit", {});      
+vkBridge.send("VKWebAppInit", {});      
     
 // Проверяет, поддерживается ли событие на текущей платформе
-if (bridge.supports("VKWebAppResizeWindow")) {
-  bridge.send("VKWebAppResizeWindow", {"width": 800, "height": 1000});
+if (vkBridge.supports("VKWebAppResizeWindow")) {
+  vkBridge.send("VKWebAppResizeWindow", {"width": 800, "height": 1000});
 }
 
 function GetName(id_fromc2)
-bridge.send('VKWebAppGetUserInfo', {
+vkBridge.send('VKWebAppGetUserInfo', {
   user_id: id_fromc2
   })
   .then((data) => { 
